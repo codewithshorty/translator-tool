39bb5ae65f115258feb7f3c7951ae906a58db97c
codewithshorty <codewithshorty@gmail.com>
2025-03-03 15:25:03 +0100

    language object added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
   const [showTranslator, setShowTranslator] = useState(false);
 
   return (
-    <div class="w-full h-screen bg-gradient-to-r from-[#051923] to-[#003554] flex justify-center items-center">
-      <div class="w-[90%] max-w-lg bg-[#006494] rounded-xl shadow-2xl shadow-gray-500 flex flex-col">
+    <div className="w-full h-screen bg-gradient-to-r from-[#051923] to-[#003554] flex justify-center items-center">
+      <div className="w-[90%] max-w-lg bg-[#006494] rounded-xl shadow-2xl shadow-gray-500 flex flex-col">
         {showTranslator ? (
           <Translator onCloseTranslator={() => setShowTranslator(false)} />
         ) : (
--- src/assets/components/Translator.jsx
+++ src/assets/components/Translator.jsx
@@ -1,39 +1,41 @@
 import React from "react";
+import { languages } from "../../languagesObject";
 
 const Translator = ({ onCloseTranslator }) => {
+  console.log(languages);
   return (
     <div>
-      <div class="p-5 space-y-5 ">
-        <div class="flex justify-between items-center ">
-          <div class="flex justify-evenly items-center w-[90%] p-1 text-white text-xl font-bold bg-gradient-to-l from-[#051923] to-[#003554] rounded-md">
+      <div className="p-5 space-y-5 ">
+        <div className="flex justify-between items-center ">
+          <div className="flex justify-evenly items-center w-[90%] p-1 text-white text-xl font-bold bg-gradient-to-l from-[#051923] to-[#003554] rounded-md">
             <h2>English</h2>
-            <i class="fa-solid fa-right-left "></i>
+            <i className="fa-solid fa-right-left "></i>
             <h2>Serbian</h2>
           </div>
           <div>
-            <button class="cursor-pointer" onClick={onCloseTranslator}>
-              <i class="fa-regular fa-rectangle-xmark text-4xl hover:text-red-800 text-red-500 font-bold"></i>
+            <button className="cursor-pointer" onClick={onCloseTranslator}>
+              <i className="fa-regular fa-rectangle-xmark text-4xl hover:text-red-800 text-red-500 font-bold"></i>
             </button>
           </div>
         </div>
 
         <div>
-          <div class="pb-3">
+          <div className="pb-3">
             <textarea
-              class="bg-(--color-5) w-full h-[25vh] rounded-md"
+              className="bg-(--color-5) w-full h-[25vh] rounded-md"
               name=""
               id=""
             ></textarea>
-            <div class="flex flex-col items-center space-y-1">
+            <div className="flex flex-col items-center space-y-1">
               <h3>100/200</h3>
-              <button class="bg-[#0582ca] text-[#051923] text-1xl font-semibold uppercase py-5 px-10 rounded-lg hover:bg-[#00a6fb] hover:text-black active:text-white translate-text ">
-                translate text <i class="fa-solid fa-turn-down"></i>
+              <button className="bg-[#0582ca] text-[#051923] text-1xl font-semibold uppercase py-5 px-10 rounded-lg hover:bg-[#00a6fb] hover:text-black active:text-white translate-text ">
+                translate text <i className="fa-solid fa-turn-down"></i>
               </button>
             </div>
           </div>
-          <div class="pt-2">
+          <div className="pt-2">
             <textarea
-              class="bg-(--color-5) w-full h-[25vh] rounded-md"
+              className="bg-(--color-5) w-full h-[25vh] rounded-md"
               name=""
               id=""
             ></textarea>
